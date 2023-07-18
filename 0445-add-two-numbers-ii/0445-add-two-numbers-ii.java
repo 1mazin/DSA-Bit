86f0c91c51ce52217ca654ae109849398dddcf50
Mehnaz Uluwar <78849132+1mazin@users.noreply.github.com>
2023-07-18 21:01:11 +0530

    Time: 2 ms (100.00%), Space: 43.2 MB (84.14%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -1,31 +1,34 @@
 public class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack<Integer> s1 = new Stack<Integer>();
-        Stack<Integer> s2 = new Stack<Integer>();
-        
-        while(l1 != null) {
-            s1.push(l1.val);
-            l1 = l1.next;
-        };
-        while(l2 != null) {
-            s2.push(l2.val);
-            l2 = l2.next;
-        }
-        
-        int totalSum = 0, carry = 0;
-        ListNode ans = new ListNode();
-        while (!s1.empty() || !s2.empty()) {
-            if (!s1.empty()) totalSum += s1.pop();
-            if (!s2.empty()) totalSum += s2.pop();
-            
-            ans.val = totalSum % 10;
-            carry = totalSum / 10;
-            ListNode head = new ListNode(carry);
-            head.next = ans;
-            ans = head;
-            totalSum = carry;
-        }
-
-        return carry == 0 ? ans.next: ans;
+    public ListNode reverse(ListNode l)
+    {
+        if(l==null || l.next==null)return l;
+        ListNode last=reverse(l.next);
+        l.next.next=l;
+        l.next=null;
+        return last;
     }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        l1= reverse(l1);
+        l2=reverse(l2);
+        int sum=0;
+        ListNode ans = new ListNode();
+        int carry=0;
+        while(l1 != null || l2!=null) {
+            if(l1!=null) {
+                sum+=l1.val;
+                l1=l1.next;
+            }
+            if(l2!=null) {
+                sum+=l2.val;
+                l2=l2.next;
+            }
+            ans.val=sum%10;
+            carry=sum/10;
+            ListNode curr=new ListNode(carry);
+            curr.next=ans;
+            ans=curr;
+            sum=carry;
+        }
+        return carry==0?ans.next:ans;
+        }
 }
