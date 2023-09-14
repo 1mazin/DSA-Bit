29fc9bcf542c9733b30085aa3be567d4f9554961
Mehnaz <78849132+1mazin@users.noreply.github.com>
2023-09-14 11:34:14 +0530

    Time: 3 ms (79.71%), Space: 43.3 MB (96.41%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,19 +1,19 @@
 class Solution {   
     public int candy(int[] ratings) {
-        int[] l2r = new int[ratings.length];
-        int[] r2l = new int[ratings.length];
-        Arrays.fill(l2r, 1);
-        Arrays.fill(r2l, 1);
+        int[] cnt = new int[ratings.length];
+
+
+        Arrays.fill(cnt, 1);
         for(int i=1;i<ratings.length;i++){
-            if(ratings[i]>ratings[i-1])l2r[i]=l2r[i-1]+1;
+            if(ratings[i]>ratings[i-1])cnt[i]=Math.max(cnt[i],cnt[i-1]+1);
         }
          for(int i=ratings.length-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1])r2l[i]=r2l[i+1]+1;
+            if(ratings[i]>ratings[i+1])cnt[i]=Math.max(cnt[i],cnt[i+1]+1);
         }
        
         int sum = 0;
         for(int i = 0 ; i < ratings.length ; i++) {
-            sum += Math.max(l2r[i], r2l[i]);
+            sum +=cnt[i];
         }
         return sum;
     }
